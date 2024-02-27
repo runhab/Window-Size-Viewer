165631cbf5bd648cc921b3a47c2776a5a0c4d499
runhab <habfenethio@gmail.com>
2024-02-27 15:55:47 +0300

    window size viewer first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+// // import logo from './logo.svg';
 import './App.css';
-
+import React, {useState,useEffect} from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight)
+  const handleResizeWidth = () => {
+    setWindowWidth(window.innerWidth)
+  }
+  const handleResizeHeight = () => {
+    setWindowHeight(window.innerHeight)
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResizeWidth)
+    window.addEventListener('resize', handleResizeHeight)
+    
+})
+ return (
+   <div class='window'>
+     <p>Your window size is {windowWidth} by {windowHeight}</p>
+     
+  </div>
+); 
 }
 
 export default App;
+// import './App.css';
+
+// import React, {useState} from 'react';
+
+// function App() {
+//   const [state, setState] = useState({count :4 , color:'blue'});
+//   const count = state.count
+//   const color = state.color
+//   function decreament() {
+//     setState(prevState => {
+//       return {...prevState ,count:prevState.count-1 }
+//     })
+//   }
+//   function increament() {
+//     setState(prevState => {
+//       return {...prevState,
+//         count:prevState.count+1
+//       }
+//     })
+//   }
+
+//   return (
+//     <div>
+//       <button onClick={decreament}>-</button>
+//       <span>{count}</span>
+//       <span>{color}</span>
+//       <button onClick={increament}>+</button>
+//     </div>
+//   );
+// }
+
+// export default App;
